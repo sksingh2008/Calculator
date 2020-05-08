40a7e1b44970b1dc927df9024a1c2a0edd5bd3d3
somesh <asdsingh2008@gmail.com>
2020-05-09 00:32:19 +0530

    Added all functions Now, all functions like subtraction, multiplication and division can be performed.

--- src/main/java/com/Calculator/Calculator.java
+++ src/main/java/com/Calculator/Calculator.java
@@ -5,23 +5,69 @@ import java.util.*;
 public class Calculator {
 
 	public static int add(int a, int b) {
-		return a + b;
+		return a+b;
+	}
+	
+	public static int sub(int a, int b) {
+		return a-b;
+	}
+	
+	public static int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public static int div(int a, int b) {
+		int result;
+		if (b == 0) {
+			throw new IllegalArgumentException("Divisor cannot divide by zero");
+		} else {
+			result = Integer.valueOf(a)/Integer.valueOf(b);
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		int choice;
+		int choice, a, b, result;
 		do {
 			System.out.println("Press 1	for addition; 2 for subtraction; 3 for multiplication; 4 for division; 0 to exit");
 			choice= sc.nextInt();
 			switch(choice)
 			{
 			case 1: System.out.println("Enter first number: ");
-					int a= sc.nextInt();
+					a= sc.nextInt();
 					System.out.println("Enter second number: ");
-					int b= sc.nextInt();
-					int result= add(a,b);
+					b= sc.nextInt();
+					result= add(a,b);
 					System.out.println("Result:"+result);
+					break;
+					
+			case 2: System.out.println("Enter first number: ");
+					a= sc.nextInt();
+					System.out.println("Enter second number: ");
+					b= sc.nextInt();
+					result= sub(a,b);
+					System.out.println("Result:"+result);
+					break;
+					
+			case 3: System.out.println("Enter first number: ");
+					a= sc.nextInt();
+					System.out.println("Enter second number: ");
+					b= sc.nextInt();
+					result= mul(a,b);
+					System.out.println("Result:"+result);
+					break;
+					
+			case 4: System.out.println("Enter first number: ");
+					a= sc.nextInt();
+					System.out.println("Enter second number: ");
+					b= sc.nextInt();
+					result= div(a,b);
+					System.out.println("Result:"+result);
+					break;
+					
+			default:if(choice!= 0)
+						System.out.println("Please give correct input");
 					break;
 					
 			}
--- src/test/java/com/Calculator/CalculatorTests.java
+++ src/test/java/com/Calculator/CalculatorTests.java
@@ -22,5 +22,37 @@ public class CalculatorTests {
         Assert.assertEquals(expectedResult, result);;
     }
     
-
+    @Test
+    public void testSubtract() {
+        int a = 25;
+        int b = 20;
+        int expectedResult = 5;
+        long result = calculator.sub(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testMultiply() {
+        int a = 10;
+        int b = 25;
+        long expectedResult = 250;
+        long result = calculator.mul(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testDivide() {
+        int a = 50;
+        int b = 10;
+        int expectedResult = 5;
+        int result = calculator.div(a, b);
+        Assert.assertEquals(expectedResult, result,0.00005);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        int a = 15;
+        int b = 0;
+        calculator.div(a, b);
+    }
 }
